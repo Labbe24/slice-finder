22198d221e5c43ab3afdcbce1236cf6bda85cd24
Lasse <lasseandresen@hotmail.com>
2022-09-02 10:52:33 +0200

    added initial PIzza model and updated README

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # slice-finder
+
+# Developer guid
+
+## App
+
+Run app
+
+```
+npm start
+```
+
+## Api
+
+Run api
+
+```
+dotnet run
+```
+
+Add new migration
+
+```
+dotnet ef migrations add [name]
+```
+
+Update database
+
+```
+dotnet ef database update
+```
+
+or
+
+```
+dotnet ef database update -v
+```
+
+for diagnosing
---
+++ api/Models/Pizza.fs
@@ -0,0 +1,23 @@
+module PizzaModel
+
+open System.ComponentModel.DataAnnotations
+open Microsoft.EntityFrameworkCore
+open EntityFrameworkCore.FSharp.Extensions
+
+[<CLIMutable>]
+type Pizza = {
+    [<Key>] Id: int
+    Name: string
+}
+
+type CustomDbContext() =  
+    inherit DbContext()
+    
+    [<DefaultValue>] val mutable pizzas : DbSet<Pizza>
+    member this.Pizzas with get() = this.pizzas and set v = this.pizzas <- v
+
+    override _.OnModelCreating builder =
+        builder.RegisterOptionTypes() // enables option values for all entities
+
+    override __.OnConfiguring(options: DbContextOptionsBuilder) : unit =
+        options.UseSqlite("Data Source=slice-finder.db") |> ignore
